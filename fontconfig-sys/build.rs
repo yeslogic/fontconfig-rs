98d644b1a1cced6aea8134256781bd4618e2081e
Wesley Moore <wes@wezm.net>
2019-11-20 17:15:20 +1100

    Go back to old build script

--- fontconfig-sys/build.rs
+++ fontconfig-sys/build.rs
@@ -1,13 +1,5 @@
 extern crate pkg_config;
 
-const FONTCONFIG_VERSION: &str = "2.11.1";
-
 fn main() {
-    pkg_config::Config::new()
-        .atleast_version(FONTCONFIG_VERSION)
-        .find("fontconfig")
-        .expect(&format!(
-            "unable to find fontconfig {} or newer with pkg-config",
-            FONTCONFIG_VERSION
-        ));
+    pkg_config::find_library("fontconfig").unwrap();
 }

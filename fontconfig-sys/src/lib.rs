53559e45938b82c82e60bc1e7ded2f62a8cf035e
Wesley Moore <wes@wezm.net>
2022-11-16 08:47:51 +1000

    Remove license ambiguity from fontconfig-sys
    The code imported from servo was licensed under the MIT licence and
    it attributed in the LICENSE file.

--- fontconfig-sys/src/lib.rs
+++ fontconfig-sys/src/lib.rs
@@ -1,11 +1,8 @@
 // Copyright 2013 The Servo Project Developers. See the LICENSE
 // file at the top-level directory of this distribution.
 //
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// Licensed under the the MIT license. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
